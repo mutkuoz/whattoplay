a45aa72c67b154f764bcc97b8fb28bc925d9d123
Mehmet Utku "Alphard" Öztürk <98752107+thealphard@users.noreply.github.com>
2023-09-03 00:37:46 +0300

    Update flask_app.py

--- flask-files/flask_app.py
+++ flask-files/flask_app.py
@@ -1,27 +1,36 @@
 import pandas as pd
 from flask import Flask, request
-import numpy as np
-data = pd.read_csv('/bgg_dataset.csv', delimiter=';')
-domains = data['Domains'].unique()
-print(domains)
+data = pd.read_csv('/home/boardgames/mysite/bgg_dataset.csv', delimiter=';')
+
 app = Flask(__name__)
 @app.route('/search', methods=['GET'])
 def ReturnBoardGames():
+    print('Category not checked yet.')
     category = request.args.get('category')
+    print(category)
     players = int(request.args.get('playerCount'))
     age = int(request.args.get('age'))
     minrate = float(request.args.get('rating'))
     if(category == "ALL"):
-        categoryFilter = True
+        games = data[
+                    (data["MinAge"] <= age) &
+                    (data['MinPlayers'] <= players) &
+                    (data['MaxPlayers'] >= players) &
+                    (data['YearPublished'] >= 1) &
+                    (data['Name'] != 'Any dummy text')
+        ] # I know this part is completely against DRY principles but it kept giving weird errors
+        print("Category is ALL")
     else:
-        categoryFilter = data['category'].isin(category)
-    games = data[
-                (data["MinAge"] >= age) &
-                (categoryFilter) &
-                (data['MinPlayers'] <= players) &
-                (data['MaxPlayers'] >= players) &
-                data['RatingAverage'] >= minrate
-    ]
+        print('Category is different')
+        games = data[
+                    (data["MinAge"] <= age) &
+                    (data['MinPlayers'] <= players) &
+                    (data['Domains'].str.contains(category)) &
+                    (data['MaxPlayers'] >= players) &
+                    (data['YearPublished'] >= 1) &
+                    (data['Name'] != 'Any dummy text')
+        ] # I know this part is completely against DRY principles but it kept giving weird errors
+
+    print(games)
     games_json = games.to_json(orient='records')
     return games_json
-    
